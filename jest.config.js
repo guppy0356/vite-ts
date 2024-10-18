099849b74f64131527344d37569a832cdbf52427
guppy0356 <guppy0356.nakira@gmail.com>
2024-10-18 23:20:42 +0900

    Migrate transformer to preset
    preset: "ts-jest" includes transformer settings

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
+  preset: "ts-jest",
 };
