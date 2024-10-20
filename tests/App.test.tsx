575d4daac91df5db47ad439d9a0f394d943381d9
guppy0356 <guppy0356.nakira@gmail.com>
2024-10-20 23:11:43 +0900

    update jest

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,10 +1,10 @@
 import {render, screen} from '@testing-library/react'
+import "@testing-library/jest-dom"
 import App from "../src/App";
 
 it("loads App component and displays title", () => {
   render(<App />);
 
-  const expectTextContent : string = "Hello Jest"
   const title : HTMLElement = screen.getByTestId("title");
-  expect(title).toHaveTextContent(expectTextContent);
+  expect(title).toHaveTextContent("Vite + React");
 });
