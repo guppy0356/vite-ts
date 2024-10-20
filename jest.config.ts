46d8bba7730f91079dd86f5b00e8fd562265f8df
guppy0356 <guppy0356.nakira@gmail.com>
2024-10-20 15:27:44 +0900

    Add jest-setup.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,11 @@ import type {Config} from 'jest';
 const config: Config = {
   testEnvironment: "node",
   preset: "ts-jest",
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.jest.json",
+    },
+  },
 };
 
 export default config;
